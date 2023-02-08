545ae7a374b857614df027f925863b74d2d33fae
Fredrik Carlsson <me@fc.io>
2023-02-08 09:38:03 +0100

    refactor index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 import chalk from 'chalk';
-import {exec} from "child_process"
-import fs from "fs"
+import {exec} from 'child_process'
+import fs from 'fs/promises'
 import util from 'util'
 import { compareAsc, format } from 'date-fns'
 import { formatDistanceToNow } from 'date-fns'
 
-const asyncExec = util.promisify(exec);
-const first = chalk.bgMagenta('Fredrik')
-const last = chalk.bgYellow('Carlsson')
-const name = `${first} ${last}`
-
+const first = 'Fredrik'
+const last = 'Carlsson'
+const name = `${chalk.bgMagenta(first)} ${chalk.bgYellow(last)}`
 // const namn2 = 'Fredrik ' + 'Carlsson'
 // const namn2 = first + ' ' + last
 // const namn2 = `${first} ${last}`
-
 console.log('name', name)
+
 // process.env.npm_config_user_agent only works when using `npm run start` not `node index.js`
 console.log(`npm & node: ${process.env.npm_config_user_agent}`)
 
-const { stdout, stderr } = await asyncExec('git --version');
-console.log(`git version: ${stdout}`);
+const asyncExec = util.promisify(exec);
+const {stdout: gitVersion} = await asyncExec('git --version');
+console.log(`git version: ${gitVersion}`);
 
-const data = `
-name: Fredrik Carlsson
+const fileContent = `
+name: ${first} ${last}
 npm & node: ${process.env.npm_config_user_agent}
-git version: ${stdout}
+git version: ${gitVersion}
 `;
-await fs.promises.writeFile("index.md", data);
+
+await fs.writeFile('index.md', fileContent);
 
 const startOfCourse = new Date(2023, 0, 31)
 console.log(formatDistanceToNow(startOfCourse))
